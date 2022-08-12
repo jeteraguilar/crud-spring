dcd9496b5584902e5aada15145af1b25d9d35bc5
Jeter Aguilar <jeteraguilar@gmail.com>
2022-08-12 14:39:37 -0300

    Formulário: API Spring:criar curso(HTTP POST)

--- src/main/java/com/jet/controller/CourseController.java
+++ src/main/java/com/jet/controller/CourseController.java
@@ -2,9 +2,12 @@ package com.jet.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jet.model.Course;
@@ -17,10 +20,17 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseController {
     
-    private CourseRepository courseRepository;    
+    private final CourseRepository courseRepository;    
 
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping 
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        courseRepository.save(course);
     }
 }
